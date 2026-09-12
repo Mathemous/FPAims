f75aeb6914a856227da1c09786b1c2c0b5db463a
Troy McCord <troymccord@hotmail.com>
2026-09-12 14:33:13 -0400

    Revert "Rebrand site as Sherry's Great Idea"
    This reverts commit 290f529ba47fa168c5bed0711bb5d5cc435e13c8.

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ function Brand({ compact = false }: { compact?: boolean }) {
   return (
     <div className={compact ? 'brand compact' : 'brand'}>
       <span>
-        Sherry&apos;s <strong>Great Idea</strong>
+        FP <strong>AIMS</strong>
       </span>
       <i />
       <p>Approved Items, Materials &amp; Services</p>
@@ -313,7 +313,7 @@ export default function Home() {
                 setEntered(true);
               }}
             >
-              Enter Sherry&apos;s Great Idea <ArrowRight size={22} />
+              Enter FP AIMS <ArrowRight size={22} />
             </a>
             <span className="welcome-steps">
               Select a program. Search. Discover.
@@ -333,7 +333,7 @@ export default function Home() {
           <a
             href="#"
             className="brand-link"
-            aria-label="Sherry's Great Idea home"
+            aria-label="FP AIMS home"
             onClick={() => setEntered(false)}
           >
             <Brand compact />
